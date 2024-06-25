7c43de12c212bfd19e71fcc294e6e4fd89eed70e
YukiTsukihara <tsukinayuki64@hotmail.com>
2024-06-24 22:28:26 -0400

    Add files via upload

---
+++ Assets/js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicialización de AOS
+    AOS.init({
+        duration: 1000,  // Duración de la animación en milisegundos (opcional, ajusta según tus necesidades)
+        once: true       // Animar elementos solo una vez mientras se desplaza
+    });
+
+    // Aquí puedes agregar cualquier otro código JavaScript que necesites para tu página
+
+});
